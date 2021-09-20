6109c08aada88c936282acbdb0d8959d36f95907
LightBlueGamer <timjonsson03@hotmail.com>
2021-09-20 11:39:55 +0200

    Merged files

--- src/game/index.js
+++ src/game/index.js
@@ -1,21 +1,20 @@
 const fs = require("fs");
 const { client } = require("../bot");
 const { Collection } = require("discord.js");
-const path = require("path");
 
 client.monsters = new Collection();
 client.items = new Collection();
 
 const monsterDir = fs.readdirSync(`${__dirname}/monsters`);
 for (const file of monsterDir) {
-  const monster = require(`${__dirname}/monsters/${file}`);
-  client.monsters.set(monster.name, monster);
-  console.log(`Loaded monster ${monster.name}`);
-};
+    const monster = require(`${__dirname}/monsters/${file}`);
+    client.monsters.set(monster.name, monster);
+    console.log(`Loaded monster ${monster.name}`);
+}
 
 const itemDir = fs.readdirSync(`${__dirname}/items`);
 for (const file of itemDir) {
-  const item = require(`${__dirname}/items/${file}`);
-  client.items.set(item.name, item);
-  console.log(`Loaded item ${item.name}`);
-};
+    const item = require(`${__dirname}/items/${file}`);
+    client.items.set(item.name, item);
+    console.log(`Loaded item ${item.name}`);
+}
